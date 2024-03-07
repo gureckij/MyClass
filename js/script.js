46acb052cc20c56754ce7fb7325ff28c31a86d11
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-07 19:16:08 +0300

    (5/9) Сохранение в переменную cardsList элемент с классом cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 /* Элементы кнопок-переключателей раскладки */
 let tileViewButtom = document.querySelector('.card-view-tile');
 let standardViewButton = document.querySelector('.card-view-standard');
+
+/* Элемент списка карточек */
+let cardsList = document.querySelector('.cards');
