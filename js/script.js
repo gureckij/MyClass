9597791b599fa176c80f94b974521980a8f2f9de
Gureckij <88166208+gureckij@users.noreply.github.com>
2024-03-07 19:11:25 +0300

    (5/9) Сохранение в переменную standardViewButton элемент с классом card-view-standard

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 /* Элементы кнопок-переключателей раскладки */
 let tileViewButtom = document.querySelector('.card-view-tile');
+let standardViewButton = document.querySelector('.card-view-standard');
